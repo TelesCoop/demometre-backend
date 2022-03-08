04ced59158eff3a5bca41b44ab23185ad0b5057a
Camille <camille@telescoop.fr>
2022-03-08 18:19:59 +0100

    [+] add csrf trusted origins

--- open_democracy_back/settings/dev.py
+++ open_democracy_back/settings/dev.py
@@ -16,3 +16,5 @@ CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = ("http://localhost:3000",)
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]

1b622a4a26201a276d01cd527f5d4594e190bbf6
kout95 <antoine.bernier@outlook.fr>
2022-03-11 17:04:52 +0100

    Ajoute epci, communes etc

--- open_democracy_back/api_views.py
+++ open_democracy_back/api_views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 
 from open_democracy_back.models import Assessment
+from open_democracy_back.data.communes import COMMUNES
 
 
 def get_assessment_view(request):
@@ -9,12 +10,18 @@ def get_assessment_view(request):
         zip_codes__zip_code=zip_code
     ).prefetch_related("zip_codes")
 
+    commune = next(
+        commune for commune in COMMUNES if zip_code in commune.get("codesPostaux", [])
+    )
+
     assessments = [
         {
             "id": assessment.id,
             "type": assessment.type,
             "type_display": assessment.get_type_display(),
             "zip_codes": [zip_code.zip_code for zip_code in assessment.zip_codes.all()],
+            "department": commune.get("departement"),
+            "population": commune.get("population"),
         }
         for assessment in assessments
     ]

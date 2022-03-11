f50e077c5ca4cb613d7de257a7d740e806355455
Antoine <6711250-Antoine95@users.noreply.gitlab.com>
2022-03-11 16:20:48 +0000

    Ajoute les communes et intercommunalités

--- open_democracy_back/models.py
+++ open_democracy_back/models.py
@@ -751,40 +751,96 @@ class AssessmentType(models.TextChoices):
 
 
 @register_snippet
-class Assessment(TimeStampedModel, ClusterableModel):
-    name = models.CharField(max_length=125, default="")
-    type = models.CharField(
-        max_length=32,
-        choices=AssessmentType.choices,
-        default=AssessmentType.MUNICIPALITY,
-    )
-
-    panels = [
-        FieldPanel("name"),
-        FieldPanel("type"),
-        InlinePanel(
-            "zip_codes",
-            label="Code postaux",
-        ),
-    ]
+class Region(models.Model):
+    code = models.CharField(max_length=3, verbose_name="Code")
+    name = models.CharField(max_length=3, verbose_name="Nom")
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Évaluation"
-        verbose_name_plural = "Évaluations"
+        verbose_name = "Région"
+        verbose_name_plural = "Régions"
 
 
-class AssessmentZipCode(Orderable):
-    assessment = ParentalKey(
-        Assessment, related_name="zip_codes", on_delete=models.CASCADE
+@register_snippet
+class Department(models.Model):
+    code = models.CharField(max_length=3, verbose_name="Code")
+    name = models.CharField(max_length=3, verbose_name="Nom")
+    region = models.ForeignKey(
+        Region, verbose_name="Région", on_delete=models.SET_NULL, null=True
     )
-    zip_code = models.CharField(max_length=32, verbose_name="Code postal")
 
     def __str__(self):
-        return self.zip_code
+        return f"{self.name} ({self.code})"
 
     class Meta:
-        verbose_name_plural = "Code postaux"
+        verbose_name = "Département"
+        verbose_name_plural = "Départements"
+
+
+@register_snippet
+class Municipality(models.Model):
+    code = models.CharField(max_length=100, verbose_name="Code insee")
+    name = models.CharField(max_length=255, verbose_name="Nom")
+    department = models.ForeignKey(
+        Department, verbose_name="Département", on_delete=models.SET_NULL, null=True
+    )
+    population = models.IntegerField(verbose_name="Population", default=0)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Commune"
+        verbose_name_plural = "Communes"
+
+
+@register_snippet
+class ZipCode(models.Model):
+    code = models.CharField(max_length=100, verbose_name="Code")
+    municipality = models.ForeignKey(
+        Municipality, verbose_name="Municipalité", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.code
+
+    class Meta:
         verbose_name = "Code postal"
+        verbose_name_plural = "Code postaux"
+
+
+@register_snippet
+class EPCI(models.Model):
+    code = models.CharField(max_length=100, verbose_name="Code insee")
+    name = models.CharField(max_length=255, verbose_name="Nom")
+    population = models.IntegerField(verbose_name="Population", default=0)
+    municipalities = models.ManyToManyField(Municipality, verbose_name="Municipalités")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Intercommunalité"
+        verbose_name_plural = "Intercommunalités"
+
+
+@register_snippet
+class Assessment(TimeStampedModel, ClusterableModel):
+    type = models.CharField(
+        max_length=32,
+        choices=AssessmentType.choices,
+        default=AssessmentType.MUNICIPALITY,
+    )
+    municipality = models.ForeignKey(
+        Municipality, blank=True, null=True, on_delete=models.SET_NULL
+    )
+    epci = models.ForeignKey(EPCI, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.type} {self.municipality}"
+
+    class Meta:
+        verbose_name = "Évaluation"
+        verbose_name_plural = "Évaluations"

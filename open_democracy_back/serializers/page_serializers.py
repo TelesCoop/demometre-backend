e3c1d3b49628f6cad0e17ef18f11a302f6ae4813
Camille <camille@telescoop.fr>
2022-03-17 16:11:01 +0100

    [*] send locale code

--- open_democracy_back/serializers/page_serializers.py
+++ open_democracy_back/serializers/page_serializers.py
@@ -3,24 +3,35 @@ from rest_framework import serializers
 from open_democracy_back.models import HomePage, EvaluationIntroPage, ReferentialPage
 
 
-PAGE_FIELDS = ["id", "title", "introduction", "locale"]
+PAGE_FIELDS = ["id", "title", "introduction", "locale_code"]
 
 
-class HomePageSerializer(serializers.ModelSerializer):
+class PageSerialiserWithLocale(serializers.ModelSerializer):
+    locale_code = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_locale_code(obj):
+        return obj.locale.language_code
+
+    class Meta:
+        abstract = True
+
+
+class HomePageSerializer(PageSerialiserWithLocale):
     class Meta:
         model = HomePage
         fields = PAGE_FIELDS
         read_only_fields = fields
 
 
-class ReferentialPageSerializer(serializers.ModelSerializer):
+class ReferentialPageSerializer(PageSerialiserWithLocale):
     class Meta:
         model = ReferentialPage
         fields = PAGE_FIELDS
         read_only_fields = fields
 
 
-class EvaluationIntroPageSerializer(serializers.ModelSerializer):
+class EvaluationIntroPageSerializer(PageSerialiserWithLocale):
     class Meta:
         model = EvaluationIntroPage
         fields = PAGE_FIELDS + ["data_consent"]

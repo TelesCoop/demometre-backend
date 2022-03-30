7262584b4e7e2e1a517577cdd5945d252e285630
Camille <camille@telescoop.fr>
2022-03-30 11:39:51 +0200

    [+] add client mail

--- open_democracy_back/settings/production.py
+++ open_democracy_back/settings/production.py
@@ -28,3 +28,11 @@ ROLLBAR = {
 }
 
 rollbar.init(**ROLLBAR)
+
+ANYMAIL = {
+    "MAILGUN_API_KEY": config.getstr("mail.api_key"),  # noqa: F405
+    "MAILGUN_SENDER_DOMAIN": "mail.telescoop.fr",
+}
+EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+DEFAULT_FROM_EMAIL = "no-reply@telescoop.fr"
+SERVER_EMAIL = "no-reply@telescoop.fr"

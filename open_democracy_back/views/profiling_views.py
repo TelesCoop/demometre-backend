e3c841ef4a560dfc94a7ba5c20d7bb13728b9b39
kout95 <antoine.bernier@outlook.fr>
2022-05-11 12:31:49 +0200

    Simplifie le back

--- open_democracy_back/views/profiling_views.py
+++ open_democracy_back/views/profiling_views.py
@@ -1,7 +1,4 @@
-from django.db.models import QuerySet
 from rest_framework import mixins, viewsets
-from rest_framework.permissions import IsAuthenticated
-from open_democracy_back.models.participation_models import Participation
 
 from open_democracy_back.models.questionnaire_and_profiling_models import (
     ProfilingQuestion,
@@ -19,23 +16,7 @@ class ProfilingQuestionView(
     viewsets.GenericViewSet,
 ):
     serializer_class = ProfilingQuestionSerializer
-
-
-class ParticipationProfilingQuestionView(
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet,
-):
-    permission_classes = [IsAuthenticated]
-    serializer_class = ProfilingQuestionSerializer
-
-    def get_queryset(self) -> QuerySet:
-        participation = Participation.objects.get(
-            id=self.kwargs.get("participation_pk")
-        )
-        population = participation.assessment.population
-        return ProfilingQuestion.objects.filter_by_role_and_population(
-            participation.role, population
-        )
+    queryset = ProfilingQuestion.objects.all()
 
 
 class RoleView(

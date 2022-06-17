15895fa8081554a5be77399bf90330c2e6ca52aa
kout95 <antoine.bernier@outlook.fr>
2022-06-17 13:02:25 +0200

    Continue de clean le code

---
+++ open_democracy_back/scoring.py
@@ -0,0 +1,202 @@
+from collections import defaultdict
+from typing import TypedDict, List
+
+import rollbar
+from django.db.models import Case, When, Value, IntegerField, Avg, FloatField, Max
+
+from open_democracy_back import settings
+from open_democracy_back.models import SCORE_MAP
+from open_democracy_back.utils import QuestionType
+
+
+class QuestionScore(TypedDict):
+    question_id: int
+    question__criteria_id: int
+    question__criteria__marker_id: int
+    question__criteria__marker__pillar_id: int
+    score: int
+
+
+def get_score_of_boolean_question(queryset) -> List[QuestionScore]:
+    result = (
+        queryset.filter(question__type=QuestionType.BOOLEAN)
+        .annotate(
+            boolean_response_int=Case(
+                When(boolean_response=True, then=Value(1)),
+                default=Value(0),
+                output_field=IntegerField(),
+            )
+        )
+        .values(
+            "question_id",
+            "question__criteria_id",
+            "question__criteria__marker_id",
+            "question__criteria__marker__pillar_id",
+        )
+        .annotate(
+            avg=Avg("boolean_response_int"),
+            score=Case(
+                When(avg__gte=0.5, then=Value(SCORE_MAP[2])),
+                default=Value(SCORE_MAP[1]),
+                output_field=FloatField(),
+            ),
+            # count=Count("question_id"),
+        )
+    )
+
+    return list(result)
+
+
+def get_score_of_unique_choice_question(queryset) -> List[QuestionScore]:
+    result = (
+        queryset.filter(question__type=QuestionType.UNIQUE_CHOICE)
+        .values(
+            "question_id",
+            "question__criteria_id",
+            "question__criteria__marker_id",
+            "question__criteria__marker__pillar_id",
+        )
+        .annotate(
+            score=Avg("unique_choice_response__linearized_score"),
+            # count=Count("question_id"),
+        )
+    )
+
+    return list(result)
+
+
+def get_score_of_multiple_choice_question(queryset) -> List[QuestionScore]:
+    multiple_choice_scores = (
+        queryset.filter(question__type=QuestionType.MULTIPLE_CHOICE)
+        .annotate(
+            multiple_choice_score_max=Max("multiple_choice_response__linearized_score")
+        )
+        .values(
+            "question_id",
+            "multiple_choice_score_max",
+            "question__criteria_id",
+            "question__criteria__marker_id",
+            "question__criteria__marker__pillar_id",
+        )
+    )
+
+    multiple_choice_score_dict = defaultdict(
+        lambda: {
+            "score": 0,
+            "count": 0,
+            "question__criteria_id": None,
+            "question__criteria__marker_id": None,
+            "question__criteria__marker__pillar_id": None,
+        }
+    )
+    for score in multiple_choice_scores:
+        multiple_choice_score_dict[score["question_id"]]["score"] += (
+            score["multiple_choice_score_max"]
+            if isinstance(score["multiple_choice_score_max"], int)
+            else 0
+        )
+        multiple_choice_score_dict[score["question_id"]][
+            "question__criteria_id"
+        ] = score["question__criteria_id"]
+        multiple_choice_score_dict[score["question_id"]][
+            "question__criteria__marker_id"
+        ] = score["question__criteria__marker_id"]
+        multiple_choice_score_dict[score["question_id"]][
+            "question__criteria__marker__pillar_id"
+        ] = score["question__criteria__marker__pillar_id"]
+        multiple_choice_score_dict[score["question_id"]]["count"] += 1
+
+    result = []
+    for key in multiple_choice_score_dict:
+        result.append(
+            {
+                "question_id": key,
+                "score": multiple_choice_score_dict[key]["score"]
+                / multiple_choice_score_dict[key]["count"],
+                "question__criteria_id": multiple_choice_score_dict[key][
+                    "question__criteria_id"
+                ],
+                "question__criteria__marker_id": multiple_choice_score_dict[key][
+                    "question__criteria__marker_id"
+                ],
+                "question__criteria__marker__pillar_id": multiple_choice_score_dict[
+                    key
+                ]["question__criteria__marker__pillar_id"],
+            }
+        )
+
+    return result
+
+
+def get_score_of_percentage_question(queryset) -> List[QuestionScore]:
+    percentage_responses = queryset.filter(
+        question__type=QuestionType.PERCENTAGE
+    ).prefetch_related("question__percentage_ranges", "question__criteria__marker")
+
+    percentage_scores_dict = defaultdict(
+        lambda: {
+            "score": 0,
+            "count": 0,
+            "question__criteria_id": None,
+            "question__criteria__marker_id": None,
+            "question__criteria__marker__pillar_id": None,
+        }
+    )
+    for response in percentage_responses:
+        score = None
+        for percentage_range in response.question.percentage_ranges.all():
+            if (
+                percentage_range.lower_bound
+                <= response.percentage_response
+                <= percentage_range.upper_bound
+            ):
+                score = percentage_range.linearized_score
+                break
+        if score is None:
+            if hasattr(settings, "production"):
+                rollbar.report_message(
+                    f"Response {response.id} of percentage question {response.question_id} don't have score",
+                    "warning",
+                )
+            continue
+
+        percentage_scores_dict[response.question_id]["score"] += score
+        percentage_scores_dict[response.question_id]["count"] += 1
+        percentage_scores_dict[response.question_id][
+            "question__criteria_id"
+        ] = response.question.criteria_id
+        percentage_scores_dict[response.question_id][
+            "question__criteria__marker_id"
+        ] = response.question.criteria.marker_id
+        percentage_scores_dict[response.question_id][
+            "question__criteria__marker__pillar_id"
+        ] = response.question.criteria.marker.pillar_id
+
+    result = []
+    for key in percentage_scores_dict:
+        result.append(
+            {
+                "question_id": key,
+                "score": percentage_scores_dict[key]["score"]
+                / percentage_scores_dict[key]["count"],
+                "count": percentage_scores_dict[key]["count"],
+                "question__criteria_id": percentage_scores_dict[key][
+                    "question__criteria_id"
+                ],
+                "question__criteria__marker_id": percentage_scores_dict[key][
+                    "question__criteria__marker_id"
+                ],
+                "question__criteria__marker__pillar_id": percentage_scores_dict[key][
+                    "question__criteria__marker__pillar_id"
+                ],
+            }
+        )
+    return result
+
+
+SCORES_FN_BY_QUESTION_TYPE = {
+    QuestionType.BOOLEAN.value: get_score_of_boolean_question,
+    QuestionType.UNIQUE_CHOICE.value: get_score_of_unique_choice_question,
+    QuestionType.MULTIPLE_CHOICE.value: get_score_of_multiple_choice_question,
+    QuestionType.PERCENTAGE.value: get_score_of_percentage_question,
+}
--- open_democracy_back/views/assessment_views.py
+++ open_democracy_back/views/assessment_views.py
@@ -1,23 +1,11 @@
 import logging
 
-from collections import defaultdict
 
 from datetime import date
 
 import numpy as np
 import pandas as pd
-import rollbar
 from django.contrib.auth.models import User
-from django.db.models import (
-    Avg,
-    Case,
-    When,
-    Value,
-    IntegerField,
-    Max,
-    Count,
-    FloatField,
-)
 from rest_framework import mixins, viewsets, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response as RestResponse
@@ -25,7 +13,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import APIException
 from rest_framework.views import APIView
 
-from open_democracy_back import settings
 from open_democracy_back.exceptions import ErrorCode, ValidationFieldError
 from open_democracy_back.mixins.update_or_create_mixin import UpdateOrCreateModelMixin
 
@@ -33,7 +20,6 @@ from open_democracy_back.models import (
     Assessment,
     ParticipationResponse,
     QuestionType,
-    SCORE_MAP,
 )
 from open_democracy_back.models.assessment_models import (
     EPCI,
@@ -47,6 +33,7 @@ from open_democracy_back.models.representativity_models import (
     RepresentativityCriteria,
 )
 from open_democracy_back.permissions import IsAssessmentAdminOrReadOnly
+from open_democracy_back.scoring import SCORES_FN_BY_QUESTION_TYPE
 from open_democracy_back.serializers.assessment_serializers import (
     AssessmentResponseSerializer,
     AssessmentSerializer,
@@ -210,230 +197,12 @@ class CompletedQuestionsInitializationView(APIView):
         return RestResponse(serializer.data, status=status.HTTP_200_OK)
 
 
-#
-# class QuestionScoreStrategy(ABC):
-#     @abstractmethod
-#     def is_question_response_type(self, rule, response: Response):
-#         pass
-#
-#
-# class UniqueChoiceStrategy(QuestionScoreStrategy):
-#     def does_respect_rule(self, rule: ProfileDefinition, response: Response):
-#         return response.unique_choice_response in rule.response_choices.all()
-#
-#
-# class MultipleChoiceStrategy(QuestionStrategy):
-#     def does_respect_rule(self, rule: ProfileDefinition, response: Response):
-#         return any(
-#             [
-#                 response in rule.response_choices.all()
-#                 for response in response.multiple_choice_response
-#             ]
-#         )
-#
-#
-# class BooleanStrategy(QuestionStrategy):
-#     def does_respect_rule(self, rule: ProfileDefinition, response: Response):
-#         return response.boolean_response == rule.boolean_response
-#
-#
-# class PercentageStrategy(QuestionStrategy):
-#     def does_respect_rule(self, rule: ProfileDefinition, response: Response):
-#         return NUMERICAL_OPERATOR_CONVERSION[rule.numerical_operator](
-#             response.percentage_response, rule.numerical_value
-#         )
-#
-#
-# RULES_STRATEGY = {
-#     "unique_choice": UniqueChoiceStrategy(),
-#     "multiple_choice": MultipleChoiceStrategy(),
-#     "boolean": BooleanStrategy(),
-#     "percentage": PercentageStrategy(),
-# }
-
-
 class QuestionnaireScoreView(APIView):
     def get(self, request, assessment_pk):
-        scores_by_question_type = {}
         assessment_participation_responses = ParticipationResponse.objects.filter(
             participation__assessment_id=assessment_pk,
             question__profiling_question=False,
         ).exclude(has_passed=True)
-        # BOOLEAN
-        scores_by_question_type[QuestionType.BOOLEAN] = (
-            assessment_participation_responses.filter(
-                question__type=QuestionType.BOOLEAN
-            )
-            .annotate(
-                boolean_response_int=Case(
-                    When(boolean_response=True, then=Value(1)),
-                    default=Value(0),
-                    output_field=IntegerField(),
-                )
-            )
-            .values(
-                "question_id",
-                "question__criteria_id",
-                "question__criteria__marker_id",
-                "question__criteria__marker__pillar_id",
-            )
-            .annotate(
-                avg=Avg("boolean_response_int"),
-                count=Count("question_id"),
-                score=Case(
-                    When(avg__gte=0.5, then=Value(SCORE_MAP[2])),
-                    default=Value(SCORE_MAP[1]),
-                    output_field=FloatField(),
-                ),
-            )
-        )
-
-        # UNIQUE_CHOICE
-        # TODO create field to determine Zscore when score change and use it there
-        scores_by_question_type[QuestionType.UNIQUE_CHOICE] = (
-            assessment_participation_responses.filter(
-                question__type=QuestionType.UNIQUE_CHOICE
-            )
-            .values(
-                "question_id",
-                "question__criteria_id",
-                "question__criteria__marker_id",
-                "question__criteria__marker__pillar_id",
-            )
-            .annotate(
-                score=Avg("unique_choice_response__linearized_score"),
-                count=Count("question_id"),
-            )
-        )
-
-        # MULTIPLE_CHOICE
-        # scores_by_question_type["multiple_choice_score"] = assessment_participation_responses.filter(question__type=QuestionType.MULTIPLE_CHOICE).annotate(multiple_choice_score_max=Greatest('multiple_choice_response__linearized_score')).values('question_id', 'multiple_choice_score_max').annotate(values=Avg('multiple_choice_score_max'))
-        multiple_choice_scores = (
-            assessment_participation_responses.filter(
-                question__type=QuestionType.MULTIPLE_CHOICE
-            )
-            .annotate(
-                multiple_choice_score_max=Max(
-                    "multiple_choice_response__linearized_score"
-                )
-            )
-            .values(
-                "question_id",
-                "multiple_choice_score_max",
-                "question__criteria_id",
-                "question__criteria__marker_id",
-                "question__criteria__marker__pillar_id",
-            )
-        )
-
-        multiple_choice_score_dict = defaultdict(
-            lambda: {
-                "score": 0,
-                "count": 0,
-                "question__criteria_id": None,
-                "question__criteria__marker_id": None,
-                "question__criteria__marker__pillar_id": None,
-            }
-        )
-        for score in multiple_choice_scores:
-            multiple_choice_score_dict[score["question_id"]]["score"] += (
-                score["multiple_choice_score_max"]
-                if isinstance(score["multiple_choice_score_max"], int)
-                else 0
-            )
-            multiple_choice_score_dict[score["question_id"]][
-                "question__criteria_id"
-            ] = score["question__criteria_id"]
-            multiple_choice_score_dict[score["question_id"]][
-                "question__criteria__marker_id"
-            ] = score["question__criteria__marker_id"]
-            multiple_choice_score_dict[score["question_id"]][
-                "question__criteria__marker__pillar_id"
-            ] = score["question__criteria__marker__pillar_id"]
-            multiple_choice_score_dict[score["question_id"]]["count"] += 1
-
-        scores_by_question_type[QuestionType.PERCENTAGE.MULTIPLE_CHOICE] = []
-        for key in multiple_choice_score_dict:
-            scores_by_question_type[QuestionType.PERCENTAGE.MULTIPLE_CHOICE].append(
-                {
-                    "question_id": key,
-                    "score": multiple_choice_score_dict[key]["score"]
-                    / multiple_choice_score_dict[key]["count"],
-                    "count": multiple_choice_score_dict[key]["count"],
-                    "question__criteria_id": multiple_choice_score_dict[key][
-                        "question__criteria_id"
-                    ],
-                    "question__criteria__marker_id": multiple_choice_score_dict[key][
-                        "question__criteria__marker_id"
-                    ],
-                    "question__criteria__marker__pillar_id": multiple_choice_score_dict[
-                        key
-                    ]["question__criteria__marker__pillar_id"],
-                }
-            )
-        # percentage
-        percentage_responses = assessment_participation_responses.filter(
-            question__type=QuestionType.PERCENTAGE
-        ).prefetch_related("question__percentage_ranges", "question__criteria__marker")
-
-        percentage_score_dict = defaultdict(
-            lambda: {
-                "score": 0,
-                "count": 0,
-                "question__criteria_id": None,
-                "question__criteria__marker_id": None,
-                "question__criteria__marker__pillar_id": None,
-            }
-        )
-        for response in percentage_responses:
-            score = None
-            for percentage_range in response.question.percentage_ranges.all():
-                if (
-                    percentage_range.lower_bound
-                    <= response.percentage_response
-                    <= percentage_range.upper_bound
-                ):
-                    score = percentage_range.linearized_score
-                    break
-            if score is None:
-                if hasattr(settings, "production"):
-                    rollbar.report_message(
-                        f"Response {response.id} of percentage question {response.question_id} don't have score",
-                        "warning",
-                    )
-                continue
-
-            percentage_score_dict[response.question_id]["score"] += score
-            percentage_score_dict[response.question_id]["count"] += 1
-            percentage_score_dict[response.question_id][
-                "question__criteria_id"
-            ] = response.question.criteria_id
-            percentage_score_dict[response.question_id][
-                "question__criteria__marker_id"
-            ] = response.question.criteria.marker_id
-            percentage_score_dict[response.question_id][
-                "question__criteria__marker__pillar_id"
-            ] = response.question.criteria.marker.pillar_id
-
-        scores_by_question_type[QuestionType.PERCENTAGE.PERCENTAGE] = []
-        for key in percentage_score_dict:
-            scores_by_question_type[QuestionType.PERCENTAGE].append(
-                {
-                    "question_id": key,
-                    "score": percentage_score_dict[key]["score"]
-                    / percentage_score_dict[key]["count"],
-                    "count": percentage_score_dict[key]["count"],
-                    "question__criteria_id": percentage_score_dict[key][
-                        "question__criteria_id"
-                    ],
-                    "question__criteria__marker_id": percentage_score_dict[key][
-                        "question__criteria__marker_id"
-                    ],
-                    "question__criteria__marker__pillar_id": percentage_score_dict[key][
-                        "question__criteria__marker__pillar_id"
-                    ],
-                }
-            )
 
         score_by_question_id = {}
         df_dict = {
@@ -450,7 +219,10 @@ class QuestionnaireScoreView(APIView):
             QuestionType.MULTIPLE_CHOICE,
             QuestionType.PERCENTAGE,
         ]:
-            for score in scores_by_question_type[question_type]:
+            question_type_scores = SCORES_FN_BY_QUESTION_TYPE[question_type.value](
+                assessment_participation_responses
+            )
+            for score in question_type_scores:
                 score_by_question_id[score["question_id"]] = score["score"]
                 df_dict["question_id"].append(score["question_id"])
                 df_dict["criteria_id"].append(score["question__criteria_id"])

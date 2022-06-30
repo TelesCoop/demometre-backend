004b14db91bcab97b4fbd58fd41c0c1c7d8aaac0
kout95 <antoine.bernier@outlook.fr>
2022-06-30 16:59:19 +0200

    Reset user and all migrations

--- my_auth/models.py
+++ my_auth/models.py
@@ -1,7 +1,11 @@
 import uuid
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
 
 
 class UserResetKey(models.Model):

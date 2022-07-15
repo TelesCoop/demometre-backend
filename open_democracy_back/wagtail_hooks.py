2416e53620af76d5683f66839a80cc53c5ce9263
kout95 <antoine.bernier@outlook.fr>
2022-07-15 16:16:05 +0200

    Déplace les rules customs

--- open_democracy_back/wagtail_hooks.py
+++ open_democracy_back/wagtail_hooks.py
@@ -46,6 +46,15 @@ from open_democracy_back.models.representativity_models import (
     RepresentativityCriteria,
 )
 from open_democracy_back.views.custom_admin_views import anomaly
+from open_democracy_back.views.wagtail_rule_views import (
+    question_intersection_operator_view,
+    question_rules_view,
+    QuestionRuleView,
+    profile_intersection_operator_view,
+    profile_definition_view,
+    ProfileDefinitionView,
+    representativity_criteria_refining_view,
+)
 
 
 class RulesButtonHelper(ButtonHelper):
@@ -417,6 +426,43 @@ modeladmin_register(UserAdmin)
 @hooks.register("register_admin_urls")
 def register_custom_admin_views():
     return [
+        # Rules
+        path(
+            "question/<int:pk>/edit-intersection-operator/",
+            question_intersection_operator_view,
+            name="question-intersection-operator",
+        ),
+        path(
+            "question/<int:pk>/rules/",
+            question_rules_view,
+            name="question-filter",
+        ),
+        path(
+            "question/<int:pk>/rule/<int:rule_pk>/delete",
+            QuestionRuleView.as_view(),
+            name="delete-question-filter",
+        ),
+        path(
+            "profile-type/<int:pk>/edit-intersection-operator/",
+            profile_intersection_operator_view,
+            name="profile-type-intersection-operator",
+        ),
+        path(
+            "profile-type/<int:pk>/rules/",
+            profile_definition_view,
+            name="profile-type-definition",
+        ),
+        path(
+            "profile-type/<int:pk>/rule/<int:rule_pk>/delete",
+            ProfileDefinitionView.as_view(),
+            name="delete-profile-type-definition",
+        ),
+        path(
+            "representativity-criteria/<int:pk>/rules/",
+            representativity_criteria_refining_view,
+            name="representativity-criteria-refining",
+        ),
+        # Anomaly
         path("anomaly/", anomaly, name="anomaly"),
     ]
 

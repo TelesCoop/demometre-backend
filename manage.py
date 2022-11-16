a49475166baac7a132944e5fb3417a704ca5be0e
kout95 <antoine.bernier@outlook.fr>
2022-11-16 10:56:14 +0100

    Fix lint

--- manage.py
+++ manage.py
@@ -4,9 +4,13 @@ import sys
 
 if __name__ == "__main__":
     if os.environ.get("TELESCOOP_DEV"):
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "open_democracy_back.settings.dev")
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "open_democracy_back.settings.dev"
+        )
     else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "open_democracy_back.settings.production")
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "open_democracy_back.settings.production"
+        )
 
     from django.core.management import execute_from_command_line
 

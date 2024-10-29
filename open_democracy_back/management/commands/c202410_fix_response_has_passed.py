87579e580eb3fcf196ad3aba3e36d3e27a5561e5
Maxime Bellec <maxime@telescoop.fr>
2024-10-29 12:16:17 +0100

    fix: add management/commands/c202410_fix_response_has_passed.py

---
+++ open_democracy_back/management/commands/c202410_fix_response_has_passed.py
@@ -0,0 +1,24 @@
+from django.core.management import BaseCommand
+from django.db.models import Q
+
+from open_democracy_back.models import ParticipationResponse
+
+
+class Command(BaseCommand):
+    help = "Fix Response.has_passed"
+
+    def handle(self, *args, **options):
+        qs = (
+            ParticipationResponse.objects.filter(has_passed=True)
+            .filter(
+                Q(unique_choice_response__isnull=False)
+                | Q(multiple_choice_response__isnull=False)
+                | Q(boolean_response__isnull=False)
+                | Q(percentage_response__isnull=True)
+                | Q(number_response__isnull=True)
+            )
+            .distinct()
+        )
+        self.stdout.write(f"Found {qs.count()} responses to fix")
+        updated = qs.update(has_passed=False)
+        self.stdout.write(f"Updated {updated} responses")
